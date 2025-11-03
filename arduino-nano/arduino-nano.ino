c4b5bc5266e59e3a0023bc84e4c91f21b972a0fb
Daedalus <agentdumbledore@gmail.com>
2025-11-03 16:51:56 -0600

    readability fixes

--- arduino-nano/arduino-nano.ino
+++ arduino-nano/arduino-nano.ino
@@ -2,7 +2,7 @@
 int clockPin = 2;
 int dataPin = 3;
 
-bool clock = false;
+bool clockState = false;
 
 const int numInputPins = 16; 
 int inputPins[numInputPins] = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, };
@@ -18,47 +18,47 @@ void setup() {
   }
 }
 
-void changeClock() {
-  clock = !clock; // Alternate clock
-  digitalWrite(clockPin, clock ? HIGH : LOW); // Update new clock
+void changeClockState() {
+  clockState = !clockState; // Alternate clock
+  digitalWrite(clockPin, clockState ? HIGH : LOW); // Update new clock
 }
 
 void loop() {
   unsigned long long output = 0; // 16 * 4 bytes each, long is 64, essentially a buffer
   
-  // Bits are read the other way
+  // First in last out, so check and insert in reverse order
   for (int i = numInputPins - 1; i >= 1; i--) {
     if (digitalRead(inputPins[i]) == HIGH) {
       output <<= 4; // Shift 4 bits
-      output |= i; // Add bytes of index
+      output |= i; // Add index bits
     }
   }
 
-  // First bytes will not show up if not explicitly set
-  bool firstOutput = false; 
+  // Zero pin's bytes alone will not show up if not explicitly set
+  bool zeroPinActive = false; 
   if (digitalRead(inputPins[0]) == HIGH) {
-    firstOutput = true;
+    zeroPinActive = true;
     output <<= 4; // Shift 4 bits
   }
 
   // Early exit if no input
-  if (output == 0 && !firstOutput) {
+  if (output == 0 && !zeroPinActive) {
     delay(waitTime);
     return;
   }
   
-  while (output != 0 || firstOutput) {
-    if (firstOutput) firstOutput = false;
+  while (output != 0 || zeroPinActive) {
+    if (zeroPinActive) zeroPinActive = false;
 
     digitalWrite(dataPin, HIGH); // Header for each nibble
-    changeClock();
+    changeClockState();
     delay(waitTime);
 
     // Process nibble(4 bits reversed)
     for (int i = 3; i >= 0; i--) {
       bool currentBit = (output & (1 << i)); // Get i in nibble
       digitalWrite(dataPin, currentBit ? HIGH : LOW); // Write data
-      changeClock();
+      changeClockState();
 
       delay(waitTime);
     }
@@ -66,6 +66,6 @@ void loop() {
   }
 
   digitalWrite(dataPin, LOW); // Set next header to false
-  changeClock();
+  changeClockState();
   delay(waitTime);
 }
