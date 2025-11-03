39d41dc0f04a8d9c333ce05e19c396be928c4ce4
Daedalus <agentdumbledore@gmail.com>
2025-11-03 16:40:36 -0600

    readability changes and compilation

--- arduino-nano/arduino-nano.ino
+++ arduino-nano/arduino-nano.ino
@@ -1,9 +1,10 @@
+
 int clockPin = 2;
 int dataPin = 3;
 
-const int numInputPins = 16; 
 bool clock = false;
 
+const int numInputPins = 16; 
 int inputPins[numInputPins] = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, };
 
 int waitTime = 50;
@@ -54,8 +55,9 @@ void loop() {
     delay(waitTime);
 
     // Process nibble(4 bits reversed)
-    for (int i = 0; i < 4; i++) {
-      digitalWrite(dataPin, (output & (1 << (3 - i))) ? HIGH : LOW); // Write data
+    for (int i = 3; i >= 0; i--) {
+      bool currentBit = (output & (1 << i)); // Get i in nibble
+      digitalWrite(dataPin, currentBit ? HIGH : LOW); // Write data
       changeClock();
 
       delay(waitTime);
